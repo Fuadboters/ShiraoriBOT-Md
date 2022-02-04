78c1b1427707ee36005b3bf4cc614c040eb74b0a
fuad <98115198+Fuadboters@users.noreply.github.com>
2022-02-04 07:13:55 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,9 +2,9 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;Ilman;;;'
-                    + 'FN:Ilman\n' // full name
-                    + 'ORG:Ilman;\n' // the organization of the contact
+                    + 'N:;Fuad;;;'
+                    + 'FN:Fuad\n' // full name
+                    + 'ORG:Fuad;\n' // the organization of the contact
                     + 'TEL;type=CELL;type=VOICE;waid=6285867185837:+62 858-6718-5837\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
                 conn.sendMessage(m.chat, { contacts: { displayName: 'Fuad', contacts: [{ vcard }] } }, { quoted: m })
